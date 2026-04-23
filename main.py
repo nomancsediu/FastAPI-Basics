ce264163a04ddf232c4efab81f8999934e4bf971
nomancsediu <abdnoman720@gmail.com>
2026-04-23 20:24:39 +0600

    Path Parameters in FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from mockData import products
 
 app = FastAPI() # Create a FastAPI instance
 
@@ -10,8 +11,29 @@ def home():
     return "Welcome to the FastAPI application!"
 
 
-@app.get("/contact") # Define a route for the contact page
+@app.get("/products") # Define a route for the /products URL
 
-# Define a function to handle requests to the contact page
-def contact():
-    return "You can connect us anytime!"
+# Define a function to handle requests to the /products URL
+def get_products():
+    return products
+
+
+#Path Params...
+# Define a route for the /products/{product_id} URL, where {product_id} is a path parameter
+@app.get("/products/{product_id}")
+def get_one_products(product_id: int):
+
+    # Loop through the products to find the one with the matching id
+    for oneProduct in products:
+        if oneProduct["id"] == product_id:
+            return oneProduct
+
+    return {
+        "error": "Product not found"
+    }
+
+
+#Query Params...
+@app.get("/greet") # Define a route for the /greet URL
+def greet_user(name:str,age:int):
+    return f"Hello, {name}! You are {age} years old."
---
+++ mockData.py
@@ -0,0 +1,14 @@
+products = [
+    {
+        "id": 1,
+        "title": "Mobile Phone",
+        "price": 10000,
+        "count": 10
+    },
+    {
+        "id": 2,
+        "title": "Apple MacBook Pro",
+        "price": 50000,
+        "count": 5
+    }
+]
