f7040d77da15637ac822dedfcd7c536bce170934
nomancsediu <abdnoman720@gmail.com>
2026-04-23 19:49:23 +0600

    First FastAPI server & GET endpoint

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-import fastapi
+from fastapi import FastAPI
 
-print(fastapi.__version__)
+app = FastAPI() # Create a FastAPI instance
+
+
+@app.get("/") # Define a route for the root URL
+
+# Define a function to handle requests to the root URL
+def home():
+    return "Welcome to the FastAPI application!"
+
+
+@app.get("/contact") # Define a route for the contact page
+
+# Define a function to handle requests to the contact page
+def contact():
+    return "You can connect us anytime!"
